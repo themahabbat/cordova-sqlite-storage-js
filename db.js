9131785da28e6f29c8d21e696774467b50dff282
themahabbatm <mahabbatmammad@yahoo.com>
2017-06-19 00:09:06 +0400

    Main file

---
+++ db.js
@@ -0,0 +1,40 @@
+var db = null;
+
+document.addEventListener('deviceready', function() {
+
+  db = window.sqlitePlugin.openDatabase({
+    name: 'notes.db',
+    location: 'default'
+  });
+
+
+  function db_query(query, values=null, success_call=null, error_call=null){
+
+    db.transaction(function(tx) {
+      tx.executeSql(query, (values!==null) ? values : []  );
+    }, function(error) {
+      if(error_call!==null)error_call(error);
+    }, function() {
+      if(success_call!==null)success_call();
+    });
+
+  }
+
+  function db_select(query, values=null, success_call=null, error_call=null){
+
+    db.transaction(function(tx){
+
+      tx.executeSql(query, (values!==null) ? values : [], function(tx, rs) {
+        if(success_call!==null)success_call(rs);
+      }, function(tx, error) {
+        if(error_call!==null)error_call(error);
+      });
+
+    });
+
+  }
+
+
+
+
+});
